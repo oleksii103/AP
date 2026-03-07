d96b12cc8e0df921c73b6a5dd87b3c0e4cb17c8d
oleksii103 <132712296+oleksii103@users.noreply.github.com>
2026-03-07 23:53:46 +0200

    lab 3.2

---
+++ lab3,2/lab3,2/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+double perimetr(double a, double b) {
+		return 2 * (a + b);
+	}
+int main() {
+	double a, b;
+	
+	std::cout << "enter side a: "; std::cin >> a;
+	std::cout << "enter side b: "; std::cin >> b;
+
+	std::cout << "perimetr = " << perimetr(a, b) << std::endl;
+}
